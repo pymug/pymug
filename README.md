7d5aaff3843d986d6818bfc9f69528a5e41fbef2
arj <arj.python@gmail.com>
2018-08-12 11:35:56 +0400

    added rendering for index.html

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ https://abdur-rahmaanj.github.io/pymug/ or http://www.pymug.com
 the site's structure is dictated by github pages' specifications and handling rather than good standards.
 
 an index.html in every folder, path or lib, is a brillant idea of github (really)
+
+# quick start
+
+no need to install jinja, just activate the virtual env
--- docs/main.py
+++ docs/main.py
@@ -1,10 +1,81 @@
 
 from jinja2 import Environment, FileSystemLoader
 
+'''
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
-template = env.get_template('index.html')
+template = env.get_template('test.html')
 
-output = template.render()
-print(output)
-        
+output = template.render(colors=['red','blue','yellow'])
+print(output, file=open('templates/output/test.html', 'w'))
+'''
+
+
+def generate(template_dir, file_in_templates, outpath, 
+    **kwargs):
+    file_loader = FileSystemLoader(template_dir)
+    env = Environment(loader=file_loader)
+    template = env.get_template(file_in_templates)
+
+    output = template.render(kwargs)
+    print(output, file=open(outpath, 'w'))
+
+sections = {
+    'events':{
+        'link':'menus/events.html',
+        'fa_class':'fa-calendar'
+    },
+    'news':{
+        'link':'menus/news.html',
+        'fa_class':'fa-newspaper-o'
+    },
+    'basic members':{
+        'link':'menus/members_basic.html',
+        'fa_class':'fa-angle-up'
+    },
+    'registered members':{
+        'link':'menus/members_reg.html',
+        'fa_class':'fa-angle-double-up'
+    },
+    'honorary members':{
+        'link':'menus/members_hon',
+        'fa_class':'fa-star'
+    },
+    'register':{
+        'link':'menus/register.html',
+        'fa_class':'fa-check'
+    },
+    'resources':{
+        'link':'menus/resources.html',
+        'fa_class':'fa-leaf'
+    },
+    'mauritius py businesses':{
+        'link':'menus/business.html',
+        'fa_class':'fa-certificate'
+    },
+    'about us':{
+        'link':'menus/about.html',
+        'fa_class':'fa-info'
+    },
+    'maintainers':{
+        'link':'menus/maintainers.html',
+        'fa_class':'fa-anchor'
+    },
+    'sponsors':{
+        'link':'menus/sponsors.html',
+        'fa_class':'fa-cc-paypal'
+    },
+    'history':{
+        'link':'menus/history.html',
+        'fa_class':'fa-book'
+    },
+    'py certif standard':{
+        'link':'menus/certifstandard.html',
+        'fa_class':'fa-file-o'
+    }
+}
+def run_main_page(**kwargs):
+    generate('templates', 'index.html', 'index.html',
+    **kwargs)
+
+run_main_page(sections=sections)
