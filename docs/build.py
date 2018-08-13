ee8d54a48627ec4acb7a39e495b351dbeb539de7
arj <arj.python@gmail.com>
2018-08-13 12:38:44 +0400

    added nav

--- docs/build.py
+++ docs/build.py
@@ -33,8 +33,8 @@ sections = {
         'link':'menus/members_basic.html',
         'fa_class':'fa-angle-up'
     },
-    'detailed members':{
-        'link':'menus/members_reg.html',
+    'evaluated members':{
+        'link':'menus/members_eval.html',
         'fa_class':'fa-angle-double-up'
     },
     'honorary members':{
@@ -68,6 +68,14 @@ sections = {
     'py certif standard':{
         'link':'menus/pystandard.html',
         'fa_class':'fa-file-o'
+    },
+    'blog':{
+        'link':'menus/blog.html',
+        'fa_class':'fa-pencil'
+    },
+    'social':{
+        'link':'menus/social.html',
+        'fa_class':'fa-paper-plane-o'
     }
 }
 def build_main_page():
@@ -104,24 +112,87 @@ def build_members_basic():
     
     # reversed_dict = collections.OrderedDict(reversed(list(basic_members.items())))
     generate('templates', 'menus/members_basic.html', 'menus/members_basic.html',
-    basic_members=basic_members, 
+    basic_members=basic_members, sections=sections, 
     page_info='basic members', year=datetime.datetime.now().year)
     print('menus/members_basic built')
 
 def build_register():
     generate('templates', 'menus/register.html', 'menus/register.html',
+        sections=sections, 
         page_info='register', year=datetime.datetime.now().year)
     print('menus/register built')
 
 def build_about():
     generate('templates', 'menus/about.html', 'menus/about.html',
+        sections=sections, 
         page_info='about', year=datetime.datetime.now().year)
     print('menus/about built')
 
 def build_pystandard():
     generate('templates', 'menus/pystandard.html', 'menus/pystandard.html',
+        sections=sections, 
         page_info='certification standard', year=datetime.datetime.now().year)
     print('menus/pystandard built')
+
+def build_blog():
+    generate('templates', 'menus/blog.html', 'menus/blog.html',
+        sections=sections, 
+        page_info='blog', year=datetime.datetime.now().year)
+    print('menus/blog built')
+
+def build_business():
+    generate('templates', 'menus/business.html', 'menus/business.html',
+        sections=sections, 
+        page_info='business', year=datetime.datetime.now().year)
+    print('menus/business built')
+
+def build_members_eval():
+    generate('templates', 'menus/members_eval.html', 'menus/members_eval.html',
+        sections=sections, 
+        page_info='evaluated members', year=datetime.datetime.now().year)
+    print('menus/members_eval built')
+
+def build_members_hon():
+    generate('templates', 'menus/members_hon.html', 'menus/members_hon.html',
+        sections=sections, 
+        page_info='honorary members', year=datetime.datetime.now().year)
+    print('menus/members_hon built')
+
+def build_news():
+    generate('templates', 'menus/news.html', 'menus/news.html',
+        sections=sections, 
+        page_info='news', year=datetime.datetime.now().year)
+    print('menus/news built')
+
+def build_social():
+    generate('templates', 'menus/social.html', 'menus/social.html',
+        sections=sections, 
+        page_info='social', year=datetime.datetime.now().year)
+    print('menus/social built')
+
+def build_resources():
+    generate('templates', 'menus/resources.html', 'menus/resources.html',
+        sections=sections, 
+        page_info='resources', year=datetime.datetime.now().year)
+    print('menus/resources built')
+
+def build_sponsors():
+    generate('templates', 'menus/sponsors.html', 'menus/sponsors.html',
+        sections=sections, 
+        page_info='sponsors', year=datetime.datetime.now().year)
+    print('menus/sponsors built')
+
+def build_events():
+    generate('templates', 'menus/events.html', 'menus/events.html',
+        sections=sections, 
+        page_info='events', year=datetime.datetime.now().year)
+    print('menus/events built')
+
+def build_maintainers():
+    generate('templates', 'menus/maintainers.html', 'menus/maintainers.html',
+        sections=sections, 
+        page_info='maintainers', year=datetime.datetime.now().year)
+    print('menus/maintainers built')
 
 def build_all():
     build_main_page()
@@ -129,6 +200,16 @@ def build_all():
     build_register()
     build_about()
     build_pystandard()
+    build_blog()
+    build_business()
+    build_members_eval()
+    build_members_hon()
+    build_news()
+    build_social()
+    build_resources()
+    build_sponsors()
+    build_events()
+    build_maintainers()
 
 if __name__ == '__main__':
     # build_main_page()
