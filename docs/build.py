29a052229666e0c8fa3990b80c3630401736b525
arj <arj.python@gmail.com>
2018-08-16 13:24:48 +0400

    heavy members work

--- docs/build.py
+++ docs/build.py
@@ -1,6 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 import collections
 import datetime
+import os
+import json
 '''
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
@@ -29,13 +31,9 @@ sections = {
         'link':'menus/news.html',
         'fa_class':'fa-newspaper-o'
     },
-    'basic members':{
+    'members':{
         'link':'menus/members_basic.html',
         'fa_class':'fa-angle-up'
-    },
-    'evaluated members':{
-        'link':'menus/members_eval.html',
-        'fa_class':'fa-angle-double-up'
     },
     'honorary members':{
         'link':'menus/members_hon.html',
@@ -113,7 +111,6 @@ def build_members_basic():
                 'date':date
             }
         # print(basic_members)
-    
     # reversed_dict = collections.OrderedDict(reversed(list(basic_members.items())))
     generate('templates', 'menus/members_basic.html', 'menus/members_basic.html',
     basic_members=basic_members, sections=sections, 
@@ -133,8 +130,16 @@ def build_about():
     print('menus/about built')
 
 def build_pystandard():
+    files = ['core_basics']
+    read_files = {}
+    for file in files:
+        with open('data/standard/{}.txt'.format(file), 'r') as stdfile:
+            read_file = stdfile.read()
+            print(read_file.count('\n'))
+            read_file = '' + read_file.replace('\n', '<hr>')
+            read_files[file] = read_file
     generate('templates', 'menus/pystandard.html', 'menus/pystandard.html',
-        sections=sections, 
+        sections=sections, read_files=read_files,
         page_info='certification standard', year=datetime.datetime.now().year)
     print('menus/pystandard built')
 
@@ -149,12 +154,6 @@ def build_business():
         sections=sections, 
         page_info='business', year=datetime.datetime.now().year)
     print('menus/business built')
-
-def build_members_eval():
-    generate('templates', 'menus/members_eval.html', 'menus/members_eval.html',
-        sections=sections, 
-        page_info='evaluated members', year=datetime.datetime.now().year)
-    print('menus/members_eval built')
 
 def build_members_hon():
     generate('templates', 'menus/members_hon.html', 'menus/members_hon.html',
@@ -204,6 +203,40 @@ def build_codeoc():
         page_info='code of conduct', year=datetime.datetime.now().year)
     print('menus/codeoc built')
 
+def build_all_members():
+    def build_member_page(uname, meminfo):
+        generate('templates', 'members/member_page.html', 'members/{}/index.html'.format(uname),
+            sections=sections, meminfo=meminfo,
+            page_info=uname, year=datetime.datetime.now().year)
+        print('members/{} built'.format(uname))
+    
+    meminfo = {}
+    with open('data/members_basic/members.txt', 'r') as memfile:
+        infos = memfile.read().splitlines()
+        for i,info in enumerate(infos):
+            data = [x.strip() for x in info.split('/')]
+            name = data[0]
+            uname = data[1]
+            date = data[2]
+            # ensures folder existence
+            try: # nice piece taken from flask source code
+                os.makedirs('members/'+uname)
+            except OSError:
+                pass
+            # create data file in folder
+            generate('templates', 'members.json', 'members/{uname}/{uname}.json'.format(uname=uname),
+            sections=sections, info={'name':name, 'date':date, 'uname':uname})
+            # use data file to generate member page
+            with open('members/{uname}/{uname}.json'.format(uname=uname)) as data_file:
+                meminfo = json.load(data_file)
+            build_member_page(uname, meminfo)
+            # reset
+            meminfo = {}
+
+
+
+
+
 def build_all():
     build_main_page()
     build_members_basic()
@@ -212,7 +245,6 @@ def build_all():
     build_pystandard()
     build_blog()
     build_business()
-    build_members_eval()
     build_members_hon()
     build_news()
     build_social()
@@ -221,6 +253,7 @@ def build_all():
     build_events()
     build_maintainers()
     build_codeoc()
+    build_all_members()
 
 if __name__ == '__main__':
     # build_main_page()
