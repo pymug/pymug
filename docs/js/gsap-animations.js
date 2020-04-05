7fa669e2a1c377a4edbd239731d21bde9f249964
kouul <maulloohans@gmail.com>
2020-04-05 09:07:32 +0400

    added animation for navbar

--- docs/js/gsap-animations.js
+++ docs/js/gsap-animations.js
@@ -1,3 +1,5 @@
-gsap.from(".letter-pymug", { duration: 0.3, x: -50, opacity: 0, scale: 1,   stagger: 0.2});
+gsap.from(".letter-pymug", { duration: 0.3, x: -50, opacity: 0, scale: 1,   stagger: 0.2, rotation:-20});
 
 gsap.from(".subheader-title", { duration: 0.3, x: 0, delay: 1.2, opacity: 0, scale: 0.7});
+
+gsap.from(".navbar-item", { duration: 0.3, x: 0, delay: 0.2, opacity: 0, scale: 0.9, stagger: 0.2});
