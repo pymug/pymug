361eed84735ce88d91ac516ec34ebfba0545c8b6
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-09-14 02:42:03 +0400

    fix: site identity

--- settings.py
+++ settings.py
@@ -9,9 +9,7 @@ info = None
 BLOG_CATEGORIES = os.listdir('./data/blog')
 
 VOLUNTEERS_DESCS = {
-    "event": "Events collaborator/programmer",
-    "code": "Website/Projects maintainer",
-    "education": "Tutorials/Posts Flask related",
+
 }
 
 # with open("info.json", encoding="utf-8") as f:
@@ -100,9 +98,9 @@ events = {
 
 info = {
     "head": {
-        "description": "Flask Community Workgroup",
-        "keywords": "python, Flask, community",
-        "author": "Flask Community",
+        "description": "Python events in Mauritius",
+        "keywords": "python, mauritius, community",
+        "author": "Python Community",
         "theme-color": "#111"
     },
     "profiles": profiles,
