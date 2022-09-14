6aaea9b511dee8ff63e156c487231ed4f1d57474
Abdur-rahmaanJ <cryptolabour@gmail.com>
2022-09-14 04:45:49 +0000

    Deploying to docs-build from @ pymug/pymug@52f983d5ea799c7a1c58e89aba8a215cd702723f 🚀

---
+++ serve.py
@@ -0,0 +1,18 @@
+import socket
+import http.server
+import socketserver
+
+# tasklist
+# /IM py37.exe /F
+#
+hostname = socket.gethostname()
+PORT = 8000
+IP = socket.gethostbyname(hostname)
+
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving on: {IP}:{PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Shutting down serve.py...")
